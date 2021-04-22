b811b40709614d6030af7649858cc027de581d30
Fliipzy <Frederiklundbeck@live.dk>
2021-04-22 09:14:19 +0200

    Added more folders & reconfigured app.js

--- src/library-nodejs/app.js
+++ src/library-nodejs/app.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json()); //json middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+const config = require("./config/appConfig.js");
+
+//express-session configuration
+const session = require("express-session");
+app.use(session(config.sessionConfig));
+
+//knex configuration
 const { Model } = require("objection");
 const Knex = require("knex");
 const knexfile = require("./knexfile.js");
-
-const serverPort = process.env.PORT || 4000;
-
 const knex = Knex(knexfile.development);
 Model.knex(knex);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
+
+//route configuration
+app.use(require("./routes/controllers/index.js"));
+app.use(require("./routes/api/auth.js"));
+
+//setup middle routing for security (comment out for easier debugging)
+/*app.use((req, res, next) => {
+    if (!req.session.user) {
+        return res.redirect('/');
+    }
+    next();
+});*/
+
+app.use(require("./routes/api/materials.js"));
+app.use(require("./routes/api/users.js"));
+
+/*
 const User = require("./models/user");
 const User_Role = require("./models/user_role");
 const User__user_role = require("./models/user__user_role");
@@ -154,6 +178,8 @@ async function testGame() {
 	}
 	testGame();
 **/
+
+const serverPort = config.port;
 
 app.listen(serverPort, (error) => {
 	if (error) {
