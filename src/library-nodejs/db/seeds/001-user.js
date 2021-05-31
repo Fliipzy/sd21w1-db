e0522917c254501e3119c683d066ef028e88ce98
Wajiddat18c <waji0003@stud.kea.dk>
2021-05-31 09:47:21 +0200

    Update 001-user.js

--- src/library-nodejs/db/seeds/001-user.js
+++ src/library-nodejs/db/seeds/001-user.js
@@ -1,7 +1,7 @@
 exports.seed = function (knex) {
   return knex('user').insert([
     { id: 1, username: 'frederik', password: '$2b$12$yXXWZD3cqaOlPA3z.fHmkep0dNSwCGqrq9NDhqvCozxwccf44lUiu' },
-    { id: 2, username: 'wajid', password: '$2y$12$H.Rtb.iZGRWSTTlvn6mCi.cITtWZeo1wd7RYhB65dCSa.VYRvPMuK' },
+    { id: 2, username: 'wajid', password: '$2b$12$wJUabhVAYCJAOMW9jD5ozOTRoLjnauPsPQwYOvYTAtwdx2HbunwVu' },
     { id: 3, username: 'jens', password: '$2y$12$QmbDQCrCckMD1UayPKvy4useWSrcOwbCv6LRhxrWl3tKvVBLCq/5K' }
   ]);
 };
